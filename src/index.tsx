7a3d7c42329df73cf466d0ba9679b1b55cc156aa
Akito_macbookpro <akito.kawamura0310@gmail.com>
2021-10-27 23:10:17 +0900

    add button

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ContainedButtons from './atoms/button';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContainedButtons />
   </React.StrictMode>,
   document.getElementById('root')
 );
